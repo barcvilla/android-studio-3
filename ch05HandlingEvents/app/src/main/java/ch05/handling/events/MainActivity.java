7ab01f031c8df525eddd3007c7785ff91fb4c0fa
Carlos Villanueva <barcvilla@gmail.com>
2018-04-27 14:34:05 -0300

    Chapter 05 - Handling Events

--- ch05HandlingEvents/app/src/main/java/ch05/handling/events/MainActivity.java
+++ ch05HandlingEvents/app/src/main/java/ch05/handling/events/MainActivity.java
@@ -1,8 +1,7 @@
-package com.example.ted.basics1;
+package ch05.handling.events;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,10 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    /*Metodo void ya que no retorna un objeto al caller. metodo sayHello igual al atributo onClick del archivo .xml*/
-    void sayHello(View v)
-    {
-        System.out.println("Hello");
     }
 }
